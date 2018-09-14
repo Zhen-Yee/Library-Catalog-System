9f694d9ac8de9502821c0183cc3459ce2991093e
Zhen-Yee <davster2@gmail.com>
2018-09-13 22:50:36 -0400

    added delete query to backend

--- server/src/main/java/com/soen343/server/controller/BookController.java
+++ server/src/main/java/com/soen343/server/controller/BookController.java
@@ -20,13 +20,15 @@ public class BookController {
     public ArrayList<Book> getBooks(){
         ArrayList<Book> listOfBook = new ArrayList<Book>();
         try {
+            // Database Connection
             Class.forName("com.mysql.jdbc.Driver");
             Connection con = DriverManager.getConnection("jdbc:mysql://testdbinstance.cwtjkaidrsfz.us-east-2.rds.amazonaws.com:3306/testdb", "test", "testtest");
-            // here sonoo is database name, root is username and password
             Statement stmt = con.createStatement();
+            
+            // SQL Query to search
             ResultSet rs = stmt.executeQuery("select * from testdb.Book");
             while (rs.next()){
-               // use the arraylist to add anonymous object retrieved from the db
+                // use the arraylist to add anonymous object retrieved from the db
                 listOfBook.add(new Book(rs.getString("name")));
                 System.out.println(rs.getString("name"));
             }
@@ -42,10 +44,10 @@ public class BookController {
         try{
         Class.forName("com.mysql.jdbc.Driver");
         Connection con = DriverManager.getConnection("jdbc:mysql://testdbinstance.cwtjkaidrsfz.us-east-2.rds.amazonaws.com:3306/testdb", "test", "testtest");
-        // here sonoo is database name, root is username and password
         Statement stmt = con.createStatement();
-        //int ex = stmt.executeUpdate("insert into new_table (name) values (5)");
-        int rs = stmt.executeUpdate("insert into testdb.Book (name) values ('"+book.name+"')");
+        
+        // SQL Query to insert
+        stmt.executeUpdate("insert into testdb.Book (name) values ('"+book.name+"')");
         con.close();
         return book;
         }
@@ -55,9 +57,20 @@ public class BookController {
         return book;
     }
 
-    @DeleteMapping("/deleteBook")
+    @PostMapping("/deleteBook")
     public Book deleteBook(@RequestBody Book book){
-        System.out.print(book.name);
+        try{
+            Class.forName("com.mysql.jdbc.Driver");
+            Connection con = DriverManager.getConnection("jdbc:mysql://testdbinstance.cwtjkaidrsfz.us-east-2.rds.amazonaws.com:3306/testdb", "test", "testtest");
+            Statement stmt = con.createStatement();
+
+            // SQL Query to delete
+            stmt.executeUpdate("DELETE FROM testdb.Book WHERE name = '"+ book.name + "'");
+            con.close();
+        }
+        catch( Exception e){
+            System.out.println(e);
+        }
         return book;
     }
 }
