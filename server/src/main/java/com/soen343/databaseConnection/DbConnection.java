5788f0fe6ed3e8d873b483c3ba6c72b99d12361a
TangMichael <tangmichael6@hotmail.com>
2018-09-22 19:35:54 -0400

    Added update function to handle POST queries

--- server/src/main/java/com/soen343/databaseConnection/DbConnection.java
+++ server/src/main/java/com/soen343/databaseConnection/DbConnection.java
@@ -3,9 +3,23 @@ package com.soen343.databaseConnection;
 import java.sql.*;
 
 public class DbConnection {
+
+    public static void update(String query){
+        try {
+            Connection con = connect();
+            // Database Connection
+            Statement stmt = con.createStatement();
+            // SQL Query to search
+            stmt.executeUpdate(query);
+            con.close();
+        } 
+        catch (Exception e) {
+            System.out.println(e);
+        } 
+    }
     
     // opens the connection to the database
-    // should only be used in this class
+    // should only be used in this class 
     public static Connection connect(){
         Connection connection = null;
         try{
