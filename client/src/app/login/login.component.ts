8bb6246fb3859accac0f55e55dce0d23160394bc
MJ <mjackson74u@gmail.com>
2018-09-26 18:38:26 -0400

    information successfully passed from form to component.ts

--- client/src/app/login/login.component.ts
+++ client/src/app/login/login.component.ts
@@ -1,8 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Router } from '@angular/router';
+//import { AppService } from "../app.service";
+import { Observable } from "rxjs";
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent {}
+export class LoginComponent {
+
+  credentials = {email: '', password: ''};
+
+  login() {
+    console.log("inputed email is" + this.credentials.email);
+    console.log("input password is" + this.credentials.password);
+  }
+}
