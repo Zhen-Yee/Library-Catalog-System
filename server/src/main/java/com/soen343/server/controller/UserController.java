8d1a3363c106bfd64a4164b430736380b5eb349f
MJ <mjackson74u@gmail.com>
2018-09-26 23:46:04 -0400

    added validateUser

--- server/src/main/java/com/soen343/server/controller/UserController.java
+++ server/src/main/java/com/soen343/server/controller/UserController.java
@@ -1,9 +1,39 @@
 package com.soen343.server.controller;
 
+import com.soen343.databaseConnection.Connector;
+import com.soen343.databaseConnection.DbConnection;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.sql.ResultSet;
 
 @RestController
 @CrossOrigin
 public class UserController {
+
+    @GetMapping("/validateUser")
+    public Boolean validateUser(String email, String password){
+
+        boolean validated = false;
+
+        try{
+
+            Connector connector = DbConnection.get(
+                    "SELECT * FROM testdb.User WHERE email_address = '" + email + "' AND password = '" + password + "'" );
+            ResultSet resultSet = connector.getResultSet();
+
+            if (resultSet.next()) {
+                validated = true;
+            }
+
+            connector.close();
+
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+
+        return validated;
+    }
+
 }
