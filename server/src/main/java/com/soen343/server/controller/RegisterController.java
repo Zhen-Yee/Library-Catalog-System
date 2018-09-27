118034dfdfd685f9ccdcb3aa5627590ca3546600
wuwentang <wuwen_tang@hotmail.com>
2018-09-27 15:51:10 -0400

    added extra files, remove when merging to dev

---
+++ server/src/main/java/com/soen343/server/controller/RegisterController.java
@@ -0,0 +1,108 @@
+package com.soen343.server.controller;
+
+import com.soen343.server.models.User;
+import com.soen343.databaseConnection.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import java.sql.*;
+import java.util.*;
+
+@RestController
+@CrossOrigin
+
+public class RegisterController {
+
+    //Puts all users into an ArrayList
+    @GetMapping("/getUser")
+    public ArrayList<User> getUsers(){
+        ArrayList<User> listOfUsers = new ArrayList<User>();
+        try{
+
+        Connector connector = DbConnection.get("select * from testdb.User");
+        ResultSet resultSet = connector.getResultSet();
+
+        while(resultSet.next()){
+        String first_name = resultSet.getString("first_name");
+        String last_name = resultSet.getString("last_name");
+        String email_address = resultSet.getString("email_address");
+        String physical_address = resultSet.getString("physical_address");
+        String phone_number = resultSet.getString("phone_number");
+        String username = resultSet.getString("username");
+        String password = resultSet.getNString("password");
+        Boolean is_admin = resultSet.getBoolean("is_admin");
+        Boolean is_online = resultSet.getBoolean("is_online");
+        User user = new User(first_name,last_name, email_address, physical_address, phone_number, username, password, is_admin, is_online);
+        listOfUsers.add(user);
+        }
+
+        }
+        catch(Exception e){
+            System.out.println(e);
+        }
+        // takes the ResultSet from the connector which are the values from the select
+        return listOfUsers;
+    }
+
+    @PostMapping("/addUser")
+    public Boolean addUser(@RequestBody User user) {
+
+        Boolean successful=false; 
+
+        try { 
+
+           if (this.doesEmailExist(user.email))
+           {
+                System.out.println("Email already exists.");
+           }
+
+           else{
+
+            successful = true;
+
+            String query = "insert into testdb.User (first_name, last_name, email_address, physical_address, phone_number, password) values ('" + user.getFirstName() + "', '"
+            + user.getLastName() + "', '" 
+            + user.email + "', '" 
+            + user.getAddress() + "', '"
+            + user.phone +"', '"
+            + user.getPassword() + "') ";
+            
+            DbConnection.update(query); 
+             
+           }
+        } catch (Exception e) {
+            System.out.println(e);
+        } 
+
+
+        return successful;
+    }
+
+    public boolean doesEmailExist(String user_email)
+    {
+
+        boolean exist = false;
+
+        try{
+
+        Connector connector = DbConnection.get("SELECT * FROM testdb.User WHERE email_address = '" + user_email + "'");
+        ResultSet resultSet = connector.getResultSet();
+    
+        if (resultSet.next() == true) {
+            exist = true;
+        }
+            
+        connector.close();
+
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+
+        return exist;
+
+
+    }
+
+}
---
+++ server/src/main/java/com/soen343/server/models/User.java
@@ -0,0 +1,59 @@
+package com.soen343.server.models;
+
+public class User {
+    private String firstName;
+    private String lastName;
+    public String email;
+    private String address;
+    public String phone;
+    private String username;
+    private String password;
+    private boolean is_admin;
+    private boolean is_active;
+
+public User(String first_name, String last_name, String email_address, 
+   String address, String phone_number, String username, String password, boolean is_admin, boolean is_online){
+        this.firstName = first_name;
+        this.lastName = last_name;
+        this.email = email_address;
+        this.address = address;
+        this.phone = phone_number;
+        this.username = username;
+        this.password = password;
+        this.is_admin = is_admin;
+        this.is_active = is_online;
+
+        System.out.println(this.email);
+    }
+    
+    public String getFirstName(){
+        return this.firstName;
+    }
+    public String getLastName(){
+        return this.lastName;
+    }
+
+    public String getEmailAddress(){
+        return this.email;
+    }
+    public String getAddress(){
+        return this.address;
+    }
+    public String getPhoneNumber(){
+        return this.phone;
+    }
+    public String getUsername(){
+        return this.username;
+    }
+    public String getPassword(){
+        return this.password;
+    }
+    public boolean getIsAdmin(){
+        return this.is_admin;
+    }
+    public boolean getIsOnline(){
+        return this.is_active;
+    }
+
+}
+ 
