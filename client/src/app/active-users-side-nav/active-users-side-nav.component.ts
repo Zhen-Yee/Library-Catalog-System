4c74cddd29a27170d6527d0a2e97c7a2937daa7e
Kevin Luu <kevinateur@hotmail.com>
2018-09-27 23:58:05 -0400

    Add see online user functionality

--- client/src/app/active-users-side-nav/active-users-side-nav.component.ts
+++ client/src/app/active-users-side-nav/active-users-side-nav.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {HttpClient} from "@angular/common/http";
 import {User} from "../../models/User.models";
-import {Observable} from 'rxjs';
 
 
 @Component({
@@ -19,16 +18,15 @@ export class ActiveUsersSideNavComponent implements OnInit {
 
     setInterval(() => {
       this.getActiveUsers();
-    }, 5000)
+    }, 2000)
 
   }
 
   getActiveUsers() {
     this.http
-      .get<User[]>("http://localhost:8090/admin/displayActiveUsers")
+      .get<User[]>("http://localhost:8090/admin/active-users")
       .subscribe(user => {
         this.activeUserArray = user;
-        console.log("HELLO " + this.activeUserArray);
       });
   }
 
