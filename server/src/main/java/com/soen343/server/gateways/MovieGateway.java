7be977dba90223e4d6795ff83137739a33a04a08
MJ <mjackson74u@gmail.com>
2018-10-15 19:00:47 -0400

    method signatures

---
+++ server/src/main/java/com/soen343/server/gateways/MovieGateway.java
@@ -0,0 +1,23 @@
+package com.soen343.server.gateways;
+
+import java.sql.ResultSet;
+
+public class MovieGateway implements CatalogItemGateway {
+
+    @Override
+    public ResultSet findAll() {
+        return null;
+    }
+
+    public void insert(){
+
+    }
+
+    public void update(){
+
+    }
+
+    public void delete(long id) {
+
+    }
+}
