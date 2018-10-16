04f44f54c94a6387f84285e420a7b9b07df191c5
MJ <mjackson74u@gmail.com>
2018-10-16 13:34:44 -0400

    Books can now be created from form

--- client/src/app/add-item/add-movies/add-movies.component.ts
+++ client/src/app/add-item/add-movies/add-movies.component.ts
@@ -1,7 +1,9 @@
 import { HttpClient } from "@angular/common/http";
 import { Component, OnInit } from "@angular/core";
 import { FormGroup, FormBuilder, Validators } from "@angular/forms";
+import {MatChipInputEvent} from "@angular/material";
 import {Movie} from "../../_models/catalog/movie.model";
+import {COMMA, ENTER} from "@angular/cdk/keycodes";
 
 @Component({
   selector: "app-add-movies",
@@ -9,40 +11,137 @@ import {Movie} from "../../_models/catalog/movie.model";
   styleUrls: ["./add-movies.component.css"]
 })
 export class AddMoviesComponent implements OnInit {
-  f: FormGroup;
-  constructor( private ff: FormBuilder, private http: HttpClient) { }
+  // input chips
+  visible = true;
+  selectable = true;
+  removable = true;
+  addOnBlur = true;
+  readonly separatorKeysCodes: number[] = [ENTER, COMMA];
+
+  producers: Array<string> = [];
+  actors: Array<string> = [];
+  subtitles: Array<string> = [];
+  dubs: Array<string> = [];
+
+  form: FormGroup;
+
+  constructor(private formBuilder: FormBuilder, private http: HttpClient) {
+  }
 
   ngOnInit() {
-    this.f = this.ff.group({
+    this.form = this.formBuilder.group({
       title: ["", Validators.required],
       director: ["", Validators.required],
-      producers: ["", Validators.required],
-      actors: ["", Validators.required],
+      // producers: [this.producers],
+      // actors: [this.actors],
       language: ["", Validators.required],
-      subtitles: ["", Validators.required],
-      dubs: ["", Validators.required],
-      releaseDate: ["", Validators.required],
+      // subtitles: [this.subtitles],
+      // dubs: [this.dubs],
+      releaseDate: ["", [Validators.required, Validators.pattern("^([0-9]{4})\\/(0[1-9]|1[0-2])\\/(0[1-9]|[1-2][0-9]|3[0-1])$")]],
       runTime: ["", Validators.required]
     });
   }
+
+  // producer, actor, subtitle, dub control
+  addProducer(event: MatChipInputEvent): void {
+    const input = event.input;
+    const value = event.value;
+
+    if ((value || '').trim()) {
+      this.producers.push(value.trim());
+    }
+
+    if (input) {
+      input.value = '';
+    }
+  }
+
+  removeProducer(producer: string): void {
+    const index = this.producers.indexOf(producer);
+
+    if (index >= 0) {
+      this.producers.splice(index, 1);
+    }
+  }
+
+  addActor(event: MatChipInputEvent): void {
+    const input = event.input;
+    const value = event.value;
+
+    if ((value || '').trim()) {
+      this.actors.push(value.trim());
+    }
+
+    if (input) {
+      input.value = '';
+    }
+  }
+
+  removeActor(actor: string): void {
+    const index = this.actors.indexOf(actor);
+
+    if (index >= 0) {
+      this.actors.splice(index, 1);
+    }
+  }
+
+  addSubtitle(event: MatChipInputEvent): void {
+    const input = event.input;
+    const value = event.value;
+
+    if ((value || '').trim()) {
+      this.subtitles.push(value.trim());
+    }
+
+    if (input) {
+      input.value = '';
+    }
+  }
+
+  removeSubtitle(sub: string): void {
+    const index = this.subtitles.indexOf(sub);
+
+    if (index >= 0) {
+      this.subtitles.splice(index, 1);
+    }
+  }
+
+  addDub(event: MatChipInputEvent): void {
+    const input = event.input;
+    const value = event.value;
+
+    if ((value || '').trim()) {
+      this.dubs.push(value.trim());
+    }
+
+    if (input) {
+      input.value = '';
+    }
+  }
+
+  removeDub(dub: string) {
+    const index = this.dubs.indexOf(dub);
+
+    if (index >= 0) {
+      this.dubs.splice(index, 1);
+    }
+  }
+
   addMovie() {
-    // dummy value used to test if it goes to backend
-    const x: Movie = {
-      ...this.f.value
-    };
-    x.title = "FrontTitleTest";
-    x.qtyInStock = 5;
-    x.qtyOnLoan = 0;
-    x.director = "testDirector";
-    x.language = "Klingon";
-    x.releaseDate = "1973/09/15";
-    x.runTime = 90;
-    x.actors = ["Actor1", "Actor2", "Actor3"];
-    x.dubs = ["Spanish", "French"];
-    x.subtitles = ["Hindu", "Greek"];
-    x.producers = ["Producer1", "Producer2", "Producer3"];
-    this.http.post("http://localhost:8090/catalog/addMovie", x)
-    .subscribe((confirmation) => console.log(confirmation));
+    if (this.form.valid) {
+      const movie: Movie = {
+        ...this.form.value
+      };
+      // requires validation to check that producers/actors/subtitles/dubs are not empty
+      movie.producers = this.producers;
+      movie.actors = this.actors;
+      movie.subtitles = this.subtitles;
+      movie.dubs = this.dubs;
+      console.log(movie);
+
+      this.http.post("http://localhost:8090/catalog/addMovie", movie)
+        .subscribe((confirmation) => console.log(confirmation));
+    }
   }
 
 }
