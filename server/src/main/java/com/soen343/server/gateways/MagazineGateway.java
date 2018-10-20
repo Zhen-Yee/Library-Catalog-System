f680fd22e5dab23c9ab6f226c54f1f628f9a62d2
Tristan <phistan007@gmail.com>
2018-10-19 21:13:50 -0400

    created the delete function in the MagazineGateway

--- server/src/main/java/com/soen343/server/gateways/MagazineGateway.java
+++ server/src/main/java/com/soen343/server/gateways/MagazineGateway.java
@@ -2,7 +2,31 @@ package com.soen343.server.gateways;
 
 import com.soen343.databaseConnection.Connector;
 
-public class MagazineGateway {
+import java.sql.*;
+import java.util.ArrayList;
+import com.soen343.databaseConnection.Connector;
+import com.soen343.databaseConnection.DbConnection;
+import com.soen343.server.models.catalog.Magazine;
 
-    private static Connector connector;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
+
+
+public class MagazineGateway {
+    public static void delete(Magazine magazine){
+        try{        
+            if(magazine.getQtyInStock() == 0){
+                String query = "DELETE FROM testdb.magazine WHERE id=" + magazine.getId();
+               DbConnection.update(query);
+            }
+            else{
+                String query = "UPDATE testdb.magazine SET qty_in_stock = '" + magazine.getQtyInStock()+ "' WHERE id = " + magazine.getId() ;
+                DbConnection.update(query); 
+            }
+        }
+        catch(Exception e){
+
+        }
+    }
 }
