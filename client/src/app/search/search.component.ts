9309db480d3a4dec1e2cbe85b18b9238b87b6bfa
wuwentang <wuwen_tang@hotmail.com>
2018-11-09 13:57:19 -0500

    refactor search html css ts

--- client/src/app/search/search.component.ts
+++ client/src/app/search/search.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { ViewChild, Component, OnInit, ElementRef } from '@angular/core';
 import { HttpClient } from "@angular/common/http";
 import { searchfilters } from "../_models/catalog/searchfilters.model";
 import { FormGroup, FormBuilder, Validators } from "@angular/forms";
@@ -7,6 +7,12 @@ import {DataService} from "../_services/DataService.service";
 import {DataTableComponent} from "../dataTable/data-table.component";
 import {CatalogItem} from "../_models/catalog/catalogItem.model";
 import {MatChipsModule} from '@angular/material/chips';
+import {COMMA, ENTER} from '@angular/cdk/keycodes';
+import {FormControl} from '@angular/forms';
+import {MatAutocompleteSelectedEvent, MatChipInputEvent, MatAutocomplete} from '@angular/material';
+import {Observable} from 'rxjs';
+import {map, startWith} from 'rxjs/operators';
+import {MatAutocompleteModule} from '@angular/material/autocomplete';
 
 @Component({
   selector: 'app-search',
@@ -18,7 +24,11 @@ export class SearchComponent implements OnInit {
   form: FormGroup;
 
     constructor(private http: HttpClient, private fb: FormBuilder, public snackBar: MatSnackBar,
-                public dataService: DataService, public dataTable: DataTableComponent) { }
+                public dataService: DataService, public dataTable: DataTableComponent) {
+        this.filteredFruits = this.fruitCtrl.valueChanges.pipe(
+        startWith(null),
+        map((fruit: string | null) => fruit ? this._filter(fruit) : this.allFruits.slice()));
+    }
 
   createForm() {
     console.log("enter Search");
@@ -76,5 +86,68 @@ export class SearchComponent implements OnInit {
     this.snackBar.open(message , action, {
       duration: 5000,
     });
+  }
+  
+  visible = true;
+  selectable = true;
+  removable = true;
+  addOnBlur = true;
+  separatorKeysCodes: number[] = [ENTER, COMMA];
+  fruitCtrl = new FormControl();
+  filteredFruits: Observable<string[]>;
+  // default value - include "title"?
+  fruits: string[] = [];
+  allFruits: string[] = ['book', 'magazine', 'music', 'movie',
+                        'title', 'author', 'publisher', 'language', 'format', 'isbn10',
+                        'type', 'artist', 'label'];
+ 
+  @ViewChild('fruitInput') fruitInput: ElementRef<HTMLInputElement>;
+  @ViewChild('auto') matAutocomplete: MatAutocomplete;
+
+  // constructor() {
+  //   this.filteredFruits = this.fruitCtrl.valueChanges.pipe(
+  //       startWith(null),
+  //       map((fruit: string | null) => fruit ? this._filter(fruit) : this.allFruits.slice()));
+  // }
+
+  add(event: MatChipInputEvent): void {
+    // Add fruit only when MatAutocomplete is not open
+    // To make sure this does not conflict with OptionSelected Event
+    if (!this.matAutocomplete.isOpen) {
+      const input = event.input;
+      const value = event.value;
+
+      // Add our fruit
+      if ((value || '').trim()) {
+        this.fruits.push(value.trim());
+      }
+
+      // Reset the input value
+      if (input) {
+        input.value = '';
+      }
+
+      this.fruitCtrl.setValue(null);
+    }
+  }
+
+  remove(fruit: string): void {
+    const index = this.fruits.indexOf(fruit);
+
+    if (index >= 0) {
+      this.fruits.splice(index, 1);
+    }
+  }
+
+  selected(event: MatAutocompleteSelectedEvent): void {
+    this.fruits.push(event.option.viewValue);
+    this.fruitInput.nativeElement.value = '';
+    this.fruitCtrl.setValue(null);
+  }
+
+  private _filter(value: string): string[] {
+    const filterValue = value.toLowerCase();
+
+    return this.allFruits.filter(fruit => fruit.toLowerCase().indexOf(filterValue) === 0);
   }
 }
