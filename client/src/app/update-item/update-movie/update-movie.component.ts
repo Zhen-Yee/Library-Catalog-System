da4333c0914d26018629c6b1f5ccf9d23d9a684f
Tamar Merdkhanian <tamar.merdkhanian@gmail.com>
2018-11-16 13:54:39 -0500

    added validation for all fields

--- client/src/app/update-item/update-movie/update-movie.component.ts
+++ client/src/app/update-item/update-movie/update-movie.component.ts
@@ -44,9 +44,7 @@ export class UpdateMovieComponent implements OnInit {
         "",
         [
           Validators.required,
-          Validators.pattern(
-            "^([0-9]{4})\\/(0[1-9]|1[0-2])\\/(0[1-9]|[1-2][0-9]|3[0-1])$"
-          )
+          Validators.pattern("^([0-9]{4})[-\/]([0-9]{2})[-\/]([0-9]{2})$")
         ]
       ],
       runTime: ["", Validators.required]
