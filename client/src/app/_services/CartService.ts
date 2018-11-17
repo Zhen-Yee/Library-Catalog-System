debff9f2a8a7ef73e8416b57714a6d74e44d5a65
Tamar Merdkhanian <tamar.merdkhanian@gmail.com>
2018-11-16 19:44:16 -0500

    fixes in cart

--- client/src/app/_services/CartService.ts
+++ client/src/app/_services/CartService.ts
@@ -14,20 +14,20 @@ export class CartService {
 
         var i;
         for (i = 0; i < this.cart.length; i++) { 
-            if (item.id == this.cart[i]["key"]["id"] && item.itemType == this.cart[i]["key"]["itemType"]){
-                if (this.cart[i]["key"]["qtyInStock"] <= this.cart[i]["value"])
+            if (item.id == this.cart[i]["catalogItem"]["id"] && item.itemType == this.cart[i]["catalogItem"]["itemType"]){
+                if (this.cart[i]["catalogItem"]["qtyInStock"] <= this.cart[i]["quantity"])
                 {
                     return "Reached limit";
                 }
-            this.cart[i]["value"]++;
-            console.log("Added "+ this.cart[i]["key"]["title"]);
+            this.cart[i]["quantity"]++;
+            console.log("Added "+ this.cart[i]["catalogItem"]["title"]);
             found = true;
         
             return "Added to Cart";}
         }
        
         if (found == false){
-        this.cart.push({key: item, value: 1});
+        this.cart.push({catalogItem: item, quantity: 1});
 
         return "Added to Cart";
     
