2809ec228bffab96cac96d11658e4c0c6959cf3d
Zhen-Yee <davster2@gmail.com>
2018-11-18 02:05:46 -0500

    display cart items in Cart Page

--- client/src/app/shopping-cart/shopping-cart.component.ts
+++ client/src/app/shopping-cart/shopping-cart.component.ts
@@ -1,27 +1,54 @@
-import { Component,  OnInit } from "@angular/core";
-import {UserService} from "../_services/user.service";
-import {CheckoutComponent} from "../checkout/checkout.component";
-import { MatDialog } from "@angular/material";
+import { Component, OnInit } from "@angular/core";
+import { UserService } from "../_services/user.service";
+import { CheckoutComponent } from "../checkout/checkout.component";
+import { MatDialog, MatTableDataSource } from "@angular/material";
+import { CartService } from '../_services/CartService';
+import { animate, state, style, transition, trigger } from "@angular/animations";
+import { CatalogItem } from "../_models/catalog/catalogItem.model";
 
 @Component({
   selector: "shopping-cart",
   templateUrl: "./shopping-cart.component.html",
   styleUrls: ["./shopping-cart.component.css"],
-  entryComponents: [CheckoutComponent]
+  entryComponents: [CheckoutComponent],
+  animations: [
+    trigger("detailExpand", [
+      state(
+        "collapsed",
+        style({ height: "0px", minHeight: "0", display: "none" })
+      ),
+      state("expanded", style({ height: "*" })),
+      transition(
+        "expanded <=> collapsed",
+        animate("225ms cubic-bezier(0.4, 0.0, 0.2, 1)")
+      )
+    ])
+  ]
 })
+
 export class ShoppingCartComponent implements OnInit {
 
-  constructor(private user: UserService, public dialog: MatDialog) {}
+  constructor(private cart: CartService, private user: UserService, public dialog: MatDialog) { }
 
-  ngOnInit() {}
+  cartArray = [];
+  dataArray: CatalogItem[] = [];
+  columnsToDisplay: string[] = ["itemType", "qtyInStock", "qtyOnLoan", "title"];
+  expandedElement: CatalogItem[];
+  dataSource: MatTableDataSource<CatalogItem>;
+
+  ngOnInit() {
+    this.cartArray = this.cart.cart
+    this.dataSource = new MatTableDataSource(this.cartArray);
+  }
 
   authenticated() {
     return this.user.authenticated;
   }
 
-  checkout()
-  {
-    this.dialog.open(CheckoutComponent);
+  checkout() {
+    console.log(this.dataSource);
+    console.log(this.cart.cart)
+   // this.dialog.open(CheckoutComponent);
   }
 
 }
