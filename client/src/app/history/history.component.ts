7b9ecd83b3f1569d8feda66f6772627d4b3b672e
wuwentang <wuwen_tang@hotmail.com>
2018-11-19 18:36:10 -0500

    path to controller for transaction history

--- client/src/app/history/history.component.ts
+++ client/src/app/history/history.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { UserService } from "../_services/user.service";
+import { HttpClient } from "@angular/common/http";
+import { FormGroup, FormBuilder, Validators } from "@angular/forms";
 
 @Component({
   selector: 'app-history',
@@ -7,9 +10,51 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HistoryComponent implements OnInit {
 
-  constructor() { }
+  toggling = false;
+
+  constructor(private http: HttpClient, private fb: FormBuilder, private user: UserService ) {
+  }
+
+  form: FormGroup;
+
+
+  createForm() {
+    console.log("enter Search");
+    this.form = this.fb.group({
+      id: ["", Validators.required],
+      userEmail: ["", Validators.required],
+      CatalogItem: ["", Validators.required],
+      checkOutDate: ["", Validators.required],
+      dueDate: ["", Validators.required],
+      returnDate: ["", Validators.required],
+    });
+  }
+
+  createAdminForm() {
+    console.log("enter Search");
+    this.form = this.fb.group({
+      dubs: ["", Validators.required],
+      releaseDate: ["", Validators.required],
+    });
+  }
+
+  isAdmin() {
+    return this.user.isAdmin;
+  }
+
 
   ngOnInit() {
+
+    this.createForm();
+    if (this.user.isAdmin) {
+      
+      this.http.get("http://localhost:8090/catalog/allTransactions").subscribe();
+
+    } else {
+
+      this.http.post("http://localhost:8090/catalog/userTransactions", this.user.userEmail).subscribe();
+
+    }
   }
 
 }
